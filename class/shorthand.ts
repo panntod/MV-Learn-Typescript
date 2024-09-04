674732cef9ee2da40a47e400bacc4a8d3baeba2d
panntod <pandhu.munjalindra@gmail.com>
2024-09-04 11:45:22 +0700

    feat: Add shorthand to create initial data

---
+++ class/shorthand.ts
@@ -0,0 +1,17 @@
+class Person {
+    // private id: number;
+    // name: string;
+
+    // tidak perlu mendeskripsikan id dan name karena sudah dideklarasikan di constructor
+    constructor(private id: number, private name: string) {
+        // this.id = id;
+        // this.name = name;
+    }
+
+    describe(this: Person) {
+        console.log('Mahasiswa dengan id: ' + this.id + ", dengan nama: " + this.name);
+    }
+}
+
+const saka = new Person(1, 'Saka');
+saka.describe(); // Mahasiswa dengan id: 1, dengan nama: Saka
